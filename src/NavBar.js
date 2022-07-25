4a1a0b971fff08c30f8cefff364fb296fb6aa567
ramaacevedo <ramiroacevedo18@hotmail.com>
2022-07-25 19:34:25 -0300

    entrega

---
+++ src/NavBar.js
@@ -0,0 +1,35 @@
+function NavBar(){
+    return(
+    <nav class="navbar navbar-expand-lg bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">L O G O</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link active" href="#">Productos</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link active" href="#">Quienes somos</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link active" href="#">Contacto</a>
+                </li>
+            </ul>
+            <form class="d-flex" role="search">
+                <input class="form-control me-2" type="search" placeholder="Buscar..." aria-label="Search" />
+                <button class="btn btn-outline-success" type="submit">Buscar</button>
+            </form>
+            </div>
+        </div>
+    </nav>
+
+    )
+}
+
+export default NavBar;
